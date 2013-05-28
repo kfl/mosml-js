9c942b3644d8de515f33d210bac74b6fa6c725c8
Henrik <henrik.bendt@gmail.com>
2013-05-28 17:52:12 +0200

    fixed the ml-syntax to js

--- src/compiler.js/tests/performance/fibBottomUp_native_intuitive.js
+++ src/compiler.js/tests/performance/fibBottomUp_native_intuitive.js
@@ -1,15 +1,15 @@
 var fib = function(n) {
-  var x = 0.0;
-  var y = 1.0;
-  while n > 0.0 {
-    tmp = y;
-    y = x+y;
-    x = y;
-    n--;
-  }
-  return x
+    var x = 0.0;
+    var y = 1.0;
+    while (n > 0.0) {
+        tmp = y;
+        y = x + y;
+        x = y;
+        n--;
+    }
+    return x;
 }
 
-var a = fib 10.0;
-var b = fib 50.0
-var c = fib 80.0
+var a = fib(10.0);
+var b = fib(50.0);
+var c = fib(80.0);
